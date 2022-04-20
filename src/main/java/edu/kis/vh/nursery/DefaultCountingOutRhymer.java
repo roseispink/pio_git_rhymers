3b5996e78ce94c770edf366e9e994229b144ece1
A-Szydlowska <235992@edu.p.lodz.pl>
2022-04-20 15:43:08 +0200

    Stałe i annotacje 4 - zmieniona nazwa zmiennej total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,33 +5,34 @@ public class DefaultCountingOutRhymer {
 	public static final int IS_EMPTY = -1;
 	public static final int CAPACITY = 12;
 	public static final int IS_FULL = CAPACITY - 1;
+	public static final int RETURN_VALUE = -1;
 	private final int[] numbers = new int[CAPACITY];
 
-	public int INITIAL = IS_EMPTY;
+	public int current = IS_EMPTY;
 
 	public void countIn(int in) {
 		if (!isFull())
-			numbers[++INITIAL] = in;
+			numbers[++current] = in;
 	}
 
 	public boolean callCheck() {
-		return INITIAL == IS_EMPTY;
+		return current == IS_EMPTY;
 	}
 
 	public boolean isFull() {
-		return INITIAL == IS_FULL;
+		return current == IS_FULL;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return IS_EMPTY;
-		return numbers[INITIAL];
+			return RETURN_VALUE;
+		return numbers[current];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return IS_EMPTY;
-		return numbers[INITIAL--];
+			return RETURN_VALUE;
+		return numbers[current--];
 	}
 
 }
