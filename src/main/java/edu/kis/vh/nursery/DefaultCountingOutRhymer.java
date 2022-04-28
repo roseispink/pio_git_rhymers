c5a5b7bd6f3692154b6d3aa180bd2e76cc1e2fc4
A-Szydlowska <235992@edu.p.lodz.pl>
2022-04-28 21:39:27 +0200

    Stałe i annotacje 5

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,33 +6,34 @@ public class DefaultCountingOutRhymer {
 	public static final int CAPACITY = 12;
 	public static final int IS_FULL = CAPACITY - 1;
 	public static final int RETURN_VALUE = -1;
+	public static final int CURRENT = -1;
 	private final int[] numbers = new int[CAPACITY];
 
-	public int current = IS_EMPTY;
+	public int total = CURRENT;
 
 	public void countIn(int in) {
 		if (!isFull())
-			numbers[++current] = in;
+			numbers[++total] = in;
 	}
 
 	public boolean callCheck() {
-		return current == IS_EMPTY;
+		return total == IS_EMPTY;
 	}
 
 	public boolean isFull() {
-		return current == IS_FULL;
+		return total == IS_FULL;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
 			return RETURN_VALUE;
-		return numbers[current];
+		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
 			return RETURN_VALUE;
-		return numbers[current--];
+		return numbers[total--];
 	}
 
 }
