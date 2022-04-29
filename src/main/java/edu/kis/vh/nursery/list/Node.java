898c361514935bd2dea2418296eb775969d9629f
weronikakus <235909@edu.p.lodz.pl>
2022-04-29 10:51:07 +0200

    hermetyzacja

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,11 +2,27 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-	int value;
-	Node prev, next;
+	private int value;
+	Node prev;
+	private Node next;
 
 	public Node(int i) {
-		value = i;
+		setValue(i);
 	}
 
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 }
